c611ceae59fa49459b0ee7cf6f607ea7a29ac66e
Daniel Yoo <daniel.y1@partner.samsung.com>
2017-06-29 17:06:03 -0700

    tutorial

--- JS/main.js
+++ JS/main.js
@@ -1,38 +1,39 @@
+var GameState = function() {
+	 screenWidth = 600;
+	 screenHeight = 800;
+	 var border;
+	 var board;
+	 var board1;
+	 numbers = new Array(6);
+	 holder = [];
+	 gameSize = screenWidth*.7;
+	 borderSize = gameSize*0.08;
+	 choosing = false;
+	 hover = null;
+	 totalScore = 0;
+	 pulse = false;
+	 timeLeft = 90;
+	 matches = 0;
 
+	 var tvMatches;
+	 var title;
+	 var current;
+	 var score;
+	 var tvChain;
+	 MAXCHAIN = 5
+	 ROWNUM = 6;
 
-var border;
-var board;
-var numbers = new Array(6);
-var holder = [];
-var screenWidth = 600;
-var screenHeight = 800;
-var gameSize = screenWidth*.7;
-var borderSize = gameSize*0.08;
-var choosing = false;
-var hover = null;
-var totalScore = 0;
-var pulse = false;
-var timeLeft = 90;
-var board1;
-var matches = 0;
+	 PRIMARY = 0x234567
+	 HOVER = 0x808080;
+	 SELECTED = 0x404040;
+	 DESELECTED = 0xFFFFFF;
 
-var tvMatches;
-var title;
-var current;
-var score;
-var tvChain;
-let MAXCHAIN = 5
-let ROWNUM = 6;
+	 SIZEMAP = {
+		'title' : 0.125,
+		'labels' : 0.06
+	}
+};
 
-let PRIMARY = 0x234567
-let HOVER = 0x808080;
-let SELECTED = 0x404040;
-let DESELECTED = 0xFFFFFF;
-
-let SIZEMAP = {
-	'title' : 0.125,
-	'labels' : 0.06
-}
 
 let COLORMAP = [];
 
@@ -46,10 +47,8 @@ let COLORMAP = {
 }
 */
 
-var GameState = {
+GameState.prototype = {
 	preload: function(){
-		this.game.scale.pageAlignHorizontally = true;
-		this.game.scale.pageAlignVertically = true;
 		this.game.scale.refresh();
 		Phaser.Canvas.setImageRenderingCrisp(this.game.canvas)  
 
@@ -66,6 +65,8 @@ var GameState = {
 		this.background.width = 500;
 		this.background.height = 500;
 		*/
+
+
 		title = this.game.add.text(game.world.width/2,SIZEMAP['title']*game.world.width/2, "S Q U A R E S", {font: SIZEMAP['title']*game.world.width+'px TestFont', fill: '#ffffff'});
 		title.anchor.setTo(0.5,0.5);
 
@@ -105,76 +106,126 @@ var GameState = {
 		for (var x = 0; x < ROWNUM; x++){
 			numbers[x] = new Array(ROWNUM);
 			for (var y = 0; y < ROWNUM; y++){
-				spawnBlock(x, y);
+				this.spawnBlock(x, y);
 			}
 		}
 
 		hover = numbers[3][3];
-		hoverBlock(hover);
+		this.hoverBlock(hover);
 
-		
+		game.input.keyboard.addKey(Phaser.Keyboard.UP).onDown.add(this.upFunction, this);
+		game.input.keyboard.addKey(Phaser.Keyboard.W).onDown.add(this.upFunction, this);
+		game.input.keyboard.addKey(Phaser.Keyboard.LEFT).onDown.add(this.leftFunction, this);
+		game.input.keyboard.addKey(Phaser.Keyboard.A).onDown.add(this.leftFunction, this);
+		game.input.keyboard.addKey(Phaser.Keyboard.DOWN).onDown.add(this.downFunction, this);
+		game.input.keyboard.addKey(Phaser.Keyboard.S).onDown.add(this.downFunction, this);
+		game.input.keyboard.addKey(Phaser.Keyboard.RIGHT).onDown.add(this.rightFunction, this);
+		game.input.keyboard.addKey(Phaser.Keyboard.D).onDown.add(this.rightFunction, this);
+		game.input.keyboard.addKey(Phaser.Keyboard.ENTER).onDown.add(this.enterFunction, this);
+		game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(this.enterFunction, this);
+		game.input.keyboard.addKey(Phaser.Keyboard.ESC).onDown.add(this.escFunction, this);
 
+		/*
 		game.input.keyboard.onDownCallback = function(){
 			if (game.input.keyboard.event.keyCode == 13||game.input.keyboard.event.keyCode == 32){
-				if (!choosing){
-					choosing = true;
-					holder.push(hover);
-					primaryBlock(hover);
-				}else{
-					choosing = false;
-					clearAllBlocks();
-					hoverBlock(hover);
-				}
 			}else{
-				if (!choosing){
-					if (hover != null){
-						if ((game.input.keyboard.event.keyCode == 87||game.input.keyboard.event.keyCode ==38)&&hover.dy>0){
-							deHover(hover);
-							hover = numbers[hover.dx][hover.dy-1];
-							hoverBlock(hover);
-						}else if ((game.input.keyboard.event.keyCode == 65 ||game.input.keyboard.event.keyCode ==37)&&hover.dx>0){
-							deHover(hover);
-							hover = numbers[hover.dx-1][hover.dy];
-							hoverBlock(hover);
-						}else if ((game.input.keyboard.event.keyCode == 83 ||game.input.keyboard.event.keyCode ==40)&&hover.dy<(ROWNUM-1)){
-							deHover(hover);
-							hover = numbers[hover.dx][hover.dy+1];
-							hoverBlock(hover);
-						}else if ((game.input.keyboard.event.keyCode == 68 ||game.input.keyboard.event.keyCode ==39)&&hover.dx<(ROWNUM-1)){
-							deHover(hover);
-							hover = numbers[hover.dx+1][hover.dy];
-							hoverBlock(hover);
-						}
-					}			
-				}else{
-					if (hover != null && holder.length < MAXCHAIN){
-						if ((game.input.keyboard.event.keyCode == 87||game.input.keyboard.event.keyCode ==38)&&hover.dy>0&&!holder.includes(numbers[hover.dx][hover.dy-1])){
-							selectBlock(hover);
-							hover = numbers[hover.dx][hover.dy-1];
-							holder.push(hover);
-							primaryBlock(hover);
-						}else if ((game.input.keyboard.event.keyCode == 65 ||game.input.keyboard.event.keyCode ==37)&&hover.dx>0&&!holder.includes(numbers[hover.dx-1][hover.dy])){
-							selectBlock(hover);
-							hover = numbers[hover.dx-1][hover.dy];
-							holder.push(hover);
-							primaryBlock(hover);
-						}else if ((game.input.keyboard.event.keyCode == 83 ||game.input.keyboard.event.keyCode ==40)&&hover.dy<(ROWNUM-1)&&!holder.includes(numbers[hover.dx][hover.dy+1])){
-							selectBlock(hover);
-							hover = numbers[hover.dx][hover.dy+1];
-							holder.push(hover);
-							primaryBlock(hover);
-						}else if ((game.input.keyboard.event.keyCode == 68 ||game.input.keyboard.event.keyCode ==39)&&hover.dx<(ROWNUM-1)&&!holder.includes(numbers[hover.dx+1][hover.dy])){
-							selectBlock(hover);
-							hover = numbers[hover.dx+1][hover.dy];
-							holder.push(hover);
-							primaryBlock(hover);
-						}
-					}	
+			}
+		}
+	*/
+
+	},
+	escFunction: function(){
+		if (choosing){
+			choosing = false;
+			this.clearBlocks();
+			this.hoverBlock(hover);
+		}
+	},
+	enterFunction: function(){
+		if (!choosing){
+			choosing = true;
+			holder.push(hover);
+			this.primaryBlock(hover);
+		}else{
+			choosing = false;
+			this.clearAllBlocks();
+			this.hoverBlock(hover);
+		}
+	},
+
+	upFunction: function(){
+		if (hover!=null &&hover.dy>0){
+			if (!choosing){
+				this.deHover(hover);
+				hover = numbers[hover.dx][hover.dy-1];
+				this.hoverBlock(hover);
+			}else{
+				if (holder.length < MAXCHAIN){
+					if (!holder.includes(numbers[hover.dx][hover.dy-1])){
+						this.selectBlock(hover);
+						hover = numbers[hover.dx][hover.dy-1];
+						holder.push(hover);
+						this.primaryBlock(hover);
+					}
 				}
 			}
 		}
-
 	},
+	leftFunction: function(){
+		if (hover!=null &&hover.dx>0){
+			if (!choosing){
+				this.deHover(hover);
+				hover = numbers[hover.dx-1][hover.dy];
+				this.hoverBlock(hover);
+			}else{
+				if (holder.length < MAXCHAIN){
+					if (!holder.includes(numbers[hover.dx-1][hover.dy])){
+						this.selectBlock(hover);
+						hover = numbers[hover.dx-1][hover.dy];
+						holder.push(hover);
+						this.primaryBlock(hover);
+					}
+				}
+			}
+		}
+	},
+	downFunction: function(){
+		if (hover!=null &&hover.dy<ROWNUM-1){
+			if (!choosing){
+				this.deHover(hover);
+				hover = numbers[hover.dx][hover.dy+1];
+				this.hoverBlock(hover);
+			}else{
+				if (holder.length < MAXCHAIN){
+					if (!holder.includes(numbers[hover.dx][hover.dy+1])){
+						this.selectBlock(hover);
+						hover = numbers[hover.dx][hover.dy+1];
+						holder.push(hover);
+						this.primaryBlock(hover);
+					}
+				}
+			}
+		}
+	},
+	rightFunction: function(){
+		if (hover!=null &&hover.dx<ROWNUM-1){
+			if (!choosing){
+				this.deHover(hover);
+				hover = numbers[hover.dx+1][hover.dy];
+				this.hoverBlock(hover);
+			}else{
+				if (holder.length < MAXCHAIN){
+					if (!holder.includes(numbers[hover.dx+1][hover.dy])){
+						this.selectBlock(hover);
+						hover = numbers[hover.dx+1][hover.dy];
+						holder.push(hover);
+						this.primaryBlock(hover);
+					}
+				}
+			}
+		}
+	},
+
 	update: function(){
 		if (choosing && hover != null){
 			if (pulse){
@@ -186,135 +237,126 @@ var GameState = {
 			}
 		}
 	},
-}
-
-function clearAllBlocks(){
-	for (var x = 0; x < holder.length; x++){
-		deHover(holder[x]);
-	}
-	if (holder.length > 1 && Number.isInteger(Math.sqrt(getTotal()))){
-		if (holder.length > Math.sqrt(getTotal())-2){
-			let phase1 = (holder.length-2)/(MAXCHAIN-2);
-			let phase2 = Math.pow(3,phase1);
-			let phase3 = Math.sqrt(getTotal());
-			let phase4 = Math.pow(phase3,phase2);
-			let plusScore = parseInt(phase4);
-
-			var addedTime = 1000*(2+Math.pow(18,((plusScore-2) / (Math.pow(7,3)-2))));
-
-			console.log(plusScore,addedTime);
-			if (addedTime<2000){
-				addedTime=2000;
-			}
-
-
-			totalScore += plusScore;
-
-			score.text = "Score: " + totalScore;
-			for (var x = 0; x < holder.length; x++){
-				var holderx = holder[x].dx;
-				var holdery = holder[x].dy;
-				holder[x].destroy();
-				var newBlock = spawnBlock(holderx, holdery,Math.sqrt(getTotal()));
-			}
-			matches +=1;
-			if (matches % 5 == 0){
-				MAXCHAIN += 1;
-			}
-			tvMatches.text = "Matches Left: "+(5-(matches%5));
-			tvChain.text = "Max Chain: "+MAXCHAIN;
-			hover = numbers[hover.dx][hover.dy];
-			hoverBlock(hover);
+	spawnBlock: function(x, y, z){
+		var newNum = this.getRandomIntInclusive(1,MAXCHAIN);
+		if (z){
+			newNum = z + 1;//to spawn the number
 		}
-	}
-	current.text = "Current: 0";
-	holder = [];
+		//var newBlock = game.add.sprite(board1.x + gameSize/6*x, board1.y + gameSize/6*y,newNum+'image');
+
+		var bmd = game.add.bitmapData(gameSize/ROWNUM, gameSize/ROWNUM);
+		 
+		bmd.ctx.beginPath();
+		bmd.ctx.rect(0, 0, gameSize/ROWNUM, gameSize/ROWNUM);
+		var fillStyle;
+		if (!((''+newNum) in COLORMAP)){
+			COLORMAP[newNum+''] =  this.generateRandomColor();
+		}
+		fillStyle = COLORMAP[newNum + ''];
+		bmd.ctx.fillStyle = fillStyle;
+		bmd.ctx.fill();
+		newBlock = game.add.sprite(board1.x + gameSize/ROWNUM*x, board1.y + gameSize/ROWNUM*y, bmd);
+
+		newBlock.value = newNum;
+		newBlock.dx = x;
+		newBlock.dy = y;
+		numbers[x][y] = newBlock;
+
+		numberLabel = this.game.add.text(newBlock.width*.5,newBlock.height*.5, newNum, {font: '35px TestFont', fill: '#ffffff'});
+		numberLabel.anchor.setTo(0.5,0.5);
+		newBlock.addChild(numberLabel);
+
+
+		return newBlock;
+	},
+	clearAllBlocks: function(){
+		for (var x = 0; x < holder.length; x++){
+			this.deHover(holder[x]);
+		}
+		if (holder.length > 1 && Number.isInteger(Math.sqrt(this.getTotal()))){
+			if (holder.length > Math.sqrt(this.getTotal())-2){
+				let phase1 = (holder.length-2)/(MAXCHAIN-2);
+				let phase2 = Math.pow(3,phase1);
+				let phase3 = Math.sqrt(this.getTotal());
+				let phase4 = Math.pow(phase3,phase2);
+				let plusScore = parseInt(phase4);
+
+				var addedTime = 1000*(2+Math.pow(18,((plusScore-2) / (Math.pow(7,3)-2))));
+
+				console.log(plusScore,addedTime);
+				if (addedTime<2000){
+					addedTime=2000;
+				}
+
+
+				totalScore += plusScore;
+
+				score.text = "Score: " + totalScore;
+				for (var x = 0; x < holder.length; x++){
+					var holderx = holder[x].dx;
+					var holdery = holder[x].dy;
+					holder[x].destroy();
+					var newBlock = this.spawnBlock(holderx, holdery,Math.sqrt(this.getTotal()));
+				}
+				matches +=1;
+				if (matches % 5 == 0){
+					MAXCHAIN += 1;
+				}
+				tvMatches.text = "Matches Left: "+(5-(matches%5));
+				tvChain.text = "Max Chain: "+MAXCHAIN;
+				hover = numbers[hover.dx][hover.dy];
+				this.hoverBlock(hover);
+			}
+		}
+		current.text = "Current: 0";
+		holder = [];
+	},
+	clearBlocks: function(){
+		for (var x = 0; x < holder.length; x++){
+			this.deHover(holder[x]);
+		}
+		current.text = "Current: 0";
+		holder = [];
+	},
+	getRandomIntInclusive: function(min, max) {
+	  min = Math.ceil(min);
+	  max = Math.floor(max);
+	  return Math.floor(Math.random() * (max - min + 1)) + min; 
+	},
+
+	getTotal: function(){
+		var tot = 0;
+		for (var x = 0; x < holder.length; x++){
+			tot = holder[x].value + tot;
+		}
+		return tot;
+	},
+	generateRandomColor: function(){
+		var r = (parseInt)((this.getRandomIntInclusive(0,255)+235)/2);
+		var b = (parseInt)((this.getRandomIntInclusive(0,255)+235)/2);
+		var g = (parseInt)((this.getRandomIntInclusive(0,255)+235)/2);
+		var test = "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+		return test;
+	},
+	rgbToHex: function(r, g, b){
+		return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
+	},
+	primaryBlock: function(block){
+		block.tint = PRIMARY;
+		current.text = "Current: "+this.getTotal() + " (" + holder.length + ")";
+	},
+
+	hoverBlock: function(block){
+		block.tint = HOVER;
+	},
+	deHover: function(block){
+		block.alpha = 1;
+		block.tint = DESELECTED;
+	},
+
+	selectBlock: function(block){
+		block.alpha = 1;
+		block.tint = SELECTED;
+	},
+
 }
-
-function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; 
-}
-
-function getTotal(){
-	var tot = 0;
-	for (var x = 0; x < holder.length; x++){
-		tot = holder[x].value + tot;
-	}
-	return tot;
-}
-
-function spawnBlock(x, y, z){
-	var newNum = getRandomIntInclusive(1,MAXCHAIN);
-	if (z){
-		newNum = z + 1;
-	}
-	//var newBlock = game.add.sprite(board1.x + gameSize/6*x, board1.y + gameSize/6*y,newNum+'image');
-
-	var bmd = game.add.bitmapData(gameSize/ROWNUM, gameSize/ROWNUM);
-	 
-	bmd.ctx.beginPath();
-	bmd.ctx.rect(0, 0, gameSize/ROWNUM, gameSize/ROWNUM);
-	var fillStyle;
-	if (!((''+newNum) in COLORMAP)){
-		COLORMAP[newNum+''] =  generateRandomColor();
-	}
-	fillStyle = COLORMAP[newNum + ''];
-	bmd.ctx.fillStyle = fillStyle;//COLORMAP[newNum+''];
-	console.log(bmd.ctx.fillStyle, newNum);
-	//console.log(COLORMAP[newNum+''],newNum);
-	bmd.ctx.fill();
-	newBlock = game.add.sprite(board1.x + gameSize/ROWNUM*x, board1.y + gameSize/ROWNUM*y, bmd);
-
-	newBlock.value = newNum;
-	newBlock.dx = x;
-	newBlock.dy = y;
-	numbers[x][y] = newBlock;
-
-	this.numberLabel = this.game.add.text(newBlock.width*.5,newBlock.height*.5, newNum, {font: '35px TestFont', fill: '#ffffff'});
-	numberLabel.anchor.setTo(0.5,0.5);
-	newBlock.addChild(numberLabel);
-
-
-	return newBlock;
-}
-
-function generateRandomColor(){
-	var r = (parseInt)((getRandomIntInclusive(0,255)+255)/2);
-	var b = (parseInt)((getRandomIntInclusive(0,255)+255)/2);
-	var g = (parseInt)((getRandomIntInclusive(0,255)+255)/2);
-	var test = "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-	console.log(test);
-	return test;
-}
-
-function rgbToHex(r, g, b){
-	return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
-}
-function primaryBlock(block){
-	block.tint = PRIMARY;
-	current.text = "Current: "+getTotal() + " (" + holder.length + ")";
-}
-
-function hoverBlock(block){
-	block.tint = HOVER;
-}
-
-function deHover(block){
-	block.alpha = 1;
-	block.tint = DESELECTED;
-}
-
-function selectBlock(block){
-	block.alpha = 1;
-	block.tint = SELECTED;
-}
-
-
-var game = new Phaser.Game(screenWidth, screenHeight, Phaser.AUTO,'',null,true);
-
-
-game.state.add('GameState', GameState);
-
-game.state.start('GameState');
